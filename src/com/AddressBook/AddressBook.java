3e5b5b34efd3fcb81592f0dbfdd78ce30efacf2f
mohdarshad1 <mdarshad181@gmail.com>
2021-04-20 20:37:29 +0530

    [Mohd Arshad] : Add UC1 Avility to Create Contact

---
+++ src/com/AddressBook/AddressBook.java
@@ -0,0 +1,18 @@
+package com.AddressBook;
+
+public class AddressBook {
+	private String first_name;
+	private String last_name;
+	private String adress;
+	private String city;
+	private String state;
+	private Long zip;
+	private String phone_number;
+	private String email_id;
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System");
+	}
+
+}
